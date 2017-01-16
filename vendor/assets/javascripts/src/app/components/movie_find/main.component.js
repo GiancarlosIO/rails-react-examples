e41dc2d9cef490cda4ad6e5abc962e13c7977876
Giancarlos <giancarlos.isasi@gmail.com>
2017-01-15 20:43:22 -0500

    Add link-router for movies-ffind app

---
+++ vendor/assets/javascripts/src/app/components/movie_find/main.component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class MovieMainComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h2>Movie component</h2>
+      </div>
+    )
+  }
+}
