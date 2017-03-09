cf7ea181d46f1e922e56c5a7a555b6fe5e436494
Giancarlos <giancarlos.isasi@gmail.com>
2017-03-09 11:20:28 -0500

    Add migrations for user and  role models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170223043321) do
+ActiveRecord::Schema.define(version: 20170309161932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,4 +36,22 @@ ActiveRecord::Schema.define(version: 20170223043321) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.decimal  "age"
+    t.integer  "role_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["role_id"], name: "index_users_on_role_id", using: :btree
+  end
+
+  add_foreign_key "users", "roles"
 end
